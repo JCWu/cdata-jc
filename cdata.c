5d49c5b707ee9ef65769709272b0223f881e27bc
jc_wu <jc_wu@pegatroncorp.com>
2012-11-18 14:08:17 +0800

    create cdata_t struct to malloc kernel memory

--- cdata.c
+++ cdata.c
@@ -7,6 +7,7 @@
 #include <linux/wait.h>
 #include <asm/io.h>
 #include "cdata_ioctl.h"
+#include <linux/slab.h> 
 
 #ifdef CONFIG_SMP
 #define __SMP__
@@ -14,10 +15,18 @@
 
 #define	CDATA_MAJOR 121 
 
+struct cdata_t {
+	char	data[1024];
+}
+
 static int cdata_open(struct inode *inode, struct file *filp)
 {
 	printk(KERN_ALERT "cdata: in cdata_open()\n");
-	printk(KERN_ALERT "The value of flip =[%d]\n", filp);
+
+	struct cdata_t *cdata;
+
+	cdata =  (struct cdata_t*)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
+	filp->private_data = (void *)data;
 	return 0;
 }
 
@@ -27,11 +36,18 @@ static int cdata_ioctl(struct inode *inode, struct file *filp,
 	printk(KERN_ALERT "cdata: in cdata_ioctl()\n");
 	switch(cmd)
 	{
+	  int i;
 	  case IOCTL_EMPTY:
 	  printk(KERN_ALERT "in ioctl:IOCTL_EMPTY\n");
+	  for(i = 0; i< 1024; i++)	
+	  	data[0] = "0"; 	
 	  return 0;
 	  case IOCTL_SYNC:
 	  printk(KERN_ALERT "in ioctl:IOCTL_SYNC\n");
+	  for(i = 0; i< 1024; i++)	
+	  {	
+		printk(KERN_ALERT "The data[%d] = (%s)\n",i, data[i]);
+	  }
 	  return 0;
 	  default:
           return -ENOTTY; 		
@@ -48,6 +64,10 @@ static ssize_t cdata_write(struct file *filp, const char *buf,
 				size_t size, loff_t *off)
 {
 	printk(KERN_ALERT "cdata_write: %s\n", buf);
+	int i;
+
+	for(i = 0; i< size; i++)	
+	  	data[i] = buf[i];
 	return 0;
 }
 
