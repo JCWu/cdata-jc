a7b4cbfa3cb1c761ae298f65721d15934d39eb94
jc_wu <jc_wu@pegatroncorp.com>
2012-11-18 14:44:56 +0800

    Test proc to test mulitthread write.

--- test.c
+++ test.c
@@ -6,12 +6,18 @@
 int main(void)
 {
 	int fd;
-	pid_t fpid;
+	pid_t child;
 
-	fpid=fork();
+	child=fork();
 
 	fd = open("/dev/cdata", O_RDWR);
+
+	if(child != 0) { // Parent
+	  write(fd,"ABCDE", 14);
+	} else {	 // Child
+	  write(fd,"12345", 14);
+	}
 	//ioctl(fd, IOCTL_EMPTY, 0);
-	//ioctl(fd, IOCTL_SYNC, 0);
+	ioctl(fd, IOCTL_SYNC, 0);
 	close(fd);
 }
